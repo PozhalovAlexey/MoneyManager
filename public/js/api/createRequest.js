9930c245f8050fd3fc2b7af4bd144fc2e3a3726b
Alexey Pozhalov <pozhalov7@mail.ru>
2023-08-03 20:34:45 +0300

    createRequest and Entity was added

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -1,7 +1,24 @@
-/**
- * Основная функция для совершения запросов
- * на сервер.
- * */
-const createRequest = (options = {}) => {
-
+const createRequest = (options = {}, callback) => {
+    const xhr = new XMLHttpRequest();
+    let formData = new FormData();
+    formData.append('name', 'demo');
+    formData.append('email', 'demo@demo');
+    formData.append('password', 'demo');
+    xhr.open('GET', 'https://localhost:8000');
+    xhr.responseType = 'json';
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            callback(null, xhr.response);
+        } else {
+            callback(new Error(`Ошибка ${xhr.status}: ${xhr.statusText}`));
+        }
+    };
+    xhr.send(formData);
 };
+createRequest({}, function (error, response) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(response);
+    }
+});
