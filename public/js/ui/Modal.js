57beaf4ab12f852888d1ca2dd6815a988991794a
Alexey Pozhalov <pozhalov7@mail.ru>
2023-10-08 18:03:04 +0300

    AccountsWidget and Modal, CreateTransactionForm was changed

--- public/js/ui/Modal.js
+++ public/js/ui/Modal.js
@@ -5,54 +5,57 @@
  * закрытие имеющихся окон
  * */
 class Modal {
-    /**
-     * Устанавливает текущий элемент в свойство element
-     * Регистрирует обработчики событий с помощью Modal.registerEvents()
-     * Если переданный элемент не существует,
-     * необходимо выкинуть ошибку.
-     * */
-    constructor(element) {
-        if (!element) {
-            throw new Error('Ошибка!!!!')
-        }
-        this.element = element;
-        this.registerEvents();
+  /**
+   * Устанавливает текущий элемент в свойство element
+   * Регистрирует обработчики событий с помощью Modal.registerEvents()
+   * Если переданный элемент не существует,
+   * необходимо выкинуть ошибку.
+   * */
+  constructor(element) {
+    if (!element) {
+      throw new Error("Ошибка!!!!");
     }
+    this.element = element;
+    this.registerEvents();
+  }
 
-    /**
-     * При нажатии на элемент с data-dismiss="modal"
-     * должен закрыть текущее окно
-     * (с помощью метода Modal.onClose)
-     * */
-    registerEvents() {
-        const elementsBtns = this.element.querySelector('button[data-dismiss="modal"]')
-        elementsBtns.addEventListener('click', (e) => {
-            e.preventDefault();
-            this.onClose(e)
-        })
+  /**
+   * При нажатии на элемент с data-dismiss="modal"
+   * должен закрыть текущее окно
+   * (с помощью метода Modal.onClose)
+   * */
+  registerEvents() {
+    const elementsBtns = this.element.querySelectorAll(
+      'button[data-dismiss="modal"]'
+    );
+    elementsBtns.forEach((e) =>
+      e.addEventListener("click", (e) => {
+        e.preventDefault();
+        this.onClose(e);
+      })
+    );
+  }
 
-    }
+  /**
+   * Срабатывает после нажатия на элементы, закрывающие окно.
+   * Закрывает текущее окно (Modal.close())
+   * */
+  onClose() {
+    this.close();
+  }
 
-    /**
-     * Срабатывает после нажатия на элементы, закрывающие окно.
-     * Закрывает текущее окно (Modal.close())
-     * */
-    onClose() {
-        this.close()
-    }
+  /**
+   * Открывает окно: устанавливает CSS-свойство display
+   * со значением «block»
+   * */
+  open() {
+    this.element.style.display = "block";
+  }
 
-    /**
-     * Открывает окно: устанавливает CSS-свойство display
-     * со значением «block»
-     * */
-    open() {
-        this.element.style.display = 'block'
-    }
-
-    /**
-     * Закрывает окно: удаляет CSS-свойство display
-     * */
-    close() {
-        this.element.removeAttribute('style')
-    }
+  /**
+   * Закрывает окно: удаляет CSS-свойство display
+   * */
+  close() {
+    this.element.removeAttribute("style");
+  }
 }
--- public/js/ui/forms/CreateTransactionForm.js
+++ public/js/ui/forms/CreateTransactionForm.js
@@ -3,53 +3,61 @@
  * создания новой транзакции
  * */
 class CreateTransactionForm extends AsyncForm {
-    /**
-     * Вызывает родительский конструктор и
-     * метод renderAccountsList
-     * */
-    constructor(element) {
-        super(element)
-        this.renderAccountsList();
-    }
+  /**
+   * Вызывает родительский конструктор и
+   * метод renderAccountsList
+   * */
+  constructor(element) {
+    super(element);
+    this.renderAccountsList();
+  }
 
-    /**
-     * Получает список счетов с помощью Account.list
-     * Обновляет в форме всплывающего окна выпадающий список
-     * */
-    renderAccountsList() {
-        const accountsSelect = this.element.querySelector('.accounts-select');
-        const accountsList = document.querySelector('.accounts-panel');
+  /**
+   * Получает список счетов с помощью Account.list
+   * Обновляет в форме всплывающего окна выпадающий список
+   * */
+  renderAccountsList() {
+    const accountsSelect = this.element.querySelector(".accounts-select");
+    let accountsList = document.querySelector(".accounts-panel");
+    const accountsPanelHeader = document.querySelector(
+      ".accounts-panel-header"
+    );
+    Account.list((err, { data, success }) => {
+      if (success) {
+        accountsSelect.innerHTML = data.reduce((html, { id, name }) => {
+          return html + `<option value="${id}">${name}</option>`;
+        }, "");
+        const menuItems = accountsList.getElementsByClassName("menu-item");
+        Array.from(menuItems).forEach((e) => e.remove());
 
-        Account.list((err, {data, success}) => {
-            if (success) {
-                accountsSelect.innerHTML = data.reduce((html, {id, name}) => {
-                    return html + `<option value="${id}">${name}</option>`;
-                }, '');
-
-                accountsList.innerHTML = data.reduce((html, {id, name}) => {
-                    return html + `<li class="menu-item"><a href="#"><span>${name}</span></a></li>`;
-                }, '');
-            }
+        data.forEach(({ id, sum, name }) => {
+          let li = document.createElement("li");
+          li.classList.add("menu-item");
+          li.innerHTML = `<a href="#">
+                    <span>${name}</span> / <span>${sum}</span> ₽
+                </a>`;
+          accountsList.appendChild(li);
         });
-    }
+      }
+    });
+  }
 
-    /**
-     * Создаёт новую транзакцию (доход или расход)
-     * с помощью Transaction.create. По успешному результату
-     * вызывает App.update(), сбрасывает форму и закрывает окно,
-     * в котором находится форма
-     * */
-    onSubmit(data) {
-        Transaction.create(data, (error, response) => {
-                this.element.reset();
-                if (response.success) {
-                    App.getModal('newIncome').close();
-                    App.getModal('newExpense').close();
-                    App.update()
-                } else {
-                    alert(response.error)
-                }
-            }
-        )
-    }
+  /**
+   * Создаёт новую транзакцию (доход или расход)
+   * с помощью Transaction.create. По успешному результату
+   * вызывает App.update(), сбрасывает форму и закрывает окно,
+   * в котором находится форма
+   * */
+  onSubmit(data) {
+    Transaction.create(data, (error, response) => {
+      this.element.reset();
+      if (response.success) {
+        App.getModal("newIncome").close();
+        App.getModal("newExpense").close();
+        App.update();
+      } else {
+        alert(response.error);
+      }
+    });
+  }
 }
